a43cd9c1238b1332169c2997de9c64fdede04cd4
theo-richard <theo.richard.contact@gmail.com>
2020-11-25 17:24:19 +0100

    First step resolving. If there is more than one number to find in the grid the program stops and display the updated grid

--- src/main/scala/sudoku/Sudoku.java
+++ src/main/scala/sudoku/Sudoku.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,6 +26,8 @@ public class Sudoku {
         printData(squares);
 
         resolveSudoku();
+//        saveResultsInFile();
+//        closeEverything();
     }
 
     public static void init(String[] args) {
@@ -58,9 +61,7 @@ public class Sudoku {
                 if (current.equals("\n")) columnCount = -1;
                 if (!current.equals("|") && !current.equals("\n") && !current.equals("+") && !current.equals("-")) {
                     columnCount++;
-//                    lines[lineCount][columnCount] = current;
                     lines.get(lineCount).add(current);
-//                    columns[columnCount][lineCount] = current;
                     columns.get(columnCount).add(current);
                     populateSquares(lineCount, columnCount, current);
                 }
@@ -95,6 +96,7 @@ public class Sudoku {
         return null;
     }
 
+    //TODO: Simplify algo
     private static void populateSquares(int lineCount, int columnCount, String value) {
         if (lineCount < 3) { // Fill in 3 first SQUARES
             if (columnCount < 3) squares.get(0).add(value);
@@ -118,40 +120,66 @@ public class Sudoku {
         }
     }
 
-    enum SHAPE {SQUARE, LINE, COLUMN}
-
     private static void resolveSudoku() {
-//        try {
-//            int i = Integer.parseInt("_");
-//        } catch (NumberFormatException e) {
-//            System.out.println("Ceci n'est pas un nombre");
-//        }
+        int missingNumbersCount;
+        for (int i = 0; i < squares.size(); i++) {
+            missingNumbersCount = Collections.frequency(squares.get(i), "_");
+
+            if (missingNumbersCount == 1) {
+                squares.get(i).set(getIndexesMissingStringsInShape(squares.get(i)).get(0), getMissingStringsInShape(squares.get(i)).get(0));
+                if (i == 1) {
+                    System.out.println("\nSQUARES:");
+                    printData(squares);
+                    break;
+                }
+            } else if (missingNumbersCount > 1) {
+                ArrayList<Integer> emptySpacesIndexes = getIndexesMissingStringsInShape(squares.get(i));
+                ArrayList<String> missingStrings = getMissingStringsInShape(squares.get(i));
+//                for (int j = 0; j < emptySpacesIndexes.size(); j++) {
+//                    for (int k = 0; k < missingStrings.size(); k++) {
+//                        if (!isInSquare(i, missingStrings.get(k)) && !isInLine(emptySpacesIndexes.get(k), missingStrings.get(k)) && isInColumn()) {
+//                            //Update maps
+//                            missingStrings.remove(k);
+//                            k = 0;
+//                            break;
+//                        }
+//                    }
+//                }
+            }
+        }
     }
 
-    public boolean isInSquare(int squareIndex, int searchingNumber) {
+    public static ArrayList<Integer> getIndexesMissingStringsInShape(ArrayList<String> values) {
+        ArrayList<Integer> indexesMissingNumbers = new ArrayList<>();
+        for (int i = 0; i < values.size(); i++) {
+            if (values.get(i).equals("_")) indexesMissingNumbers.add(i);
+        }
+        return indexesMissingNumbers;
+    }
+
+    public static ArrayList<String> getMissingStringsInShape(ArrayList<String> values) {
+        ArrayList<String> missingNumbers = new ArrayList<>();
+        for (int i = 0; i < values.size(); i++) {
+            if (!values.contains(String.valueOf(i + 1))) missingNumbers.add(String.valueOf(i + 1));
+        }
+        return missingNumbers;
+    }
+
+    public static boolean isInSquare(int squareIndex, String searchingNumber) {
         ArrayList<String> values = squares.get(squareIndex);
-        for (String s : values) {
-            int i = Integer.parseInt(s);
-            if (i == searchingNumber) return true;
-        }
+        for (String s : values) if (s.equals(searchingNumber)) return true;
         return false;
     }
 
-    public boolean isInLine(int lineIndex, int searchingNumber) {
+    public static boolean isInLine(int lineIndex, String searchingNumber) {
         ArrayList<String> values = lines.get(lineIndex);
-        for (String s : values) {
-            int i = Integer.parseInt(s);
-            if (i == searchingNumber) return true;
-        }
+        for (String s : values) if (s.equals(searchingNumber)) return true;
         return false;
     }
 
-    public boolean isInColumn(int columnIndex, int searchingNumber) {
+    public static boolean isInColumn(int columnIndex, String searchingNumber) {
         ArrayList<String> values = columns.get(columnIndex);
-        for (String s : values) {
-            int i = Integer.parseInt(s);
-            if (i == searchingNumber) return true;
-        }
+        for (String s : values) if (s.equals(searchingNumber)) return true;
         return false;
     }
 }
