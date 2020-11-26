1e39b45034826d6872a1cc5e77d98707372b4229
theo-richard <theo.richard.contact@gmail.com>
2020-11-26 17:51:04 +0100

    Use BigInteger to handle enormous numbers and use US format to add comas each at each thousands.

--- src/main/scala/facto/big/BigFacto.java
+++ src/main/scala/facto/big/BigFacto.java
@@ -1,12 +1,20 @@
 package facto.big;
 
+import java.math.BigInteger;
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class BigFacto {
     public static void main(String[] args) {
-        System.out.println("!" + args[0] + " = " + facto(Integer.parseInt(args[0])));
+        BigInteger bi = facto(new BigInteger(args[0]));
+        String formattedResult = NumberFormat.getNumberInstance(Locale.US).format(bi);
+        System.out.println("!" + args[0] + " = " + formattedResult);
     }
 
-    public static int facto(int entry) {
-        if (entry <= 1) return 1;
-        else return entry * facto(entry - 1);
+    public static BigInteger facto(BigInteger entry) {
+        if (entry.intValue() <= 1) return new BigInteger("1");
+        else return entry.multiply(
+                facto(entry.subtract(new BigInteger("1")))
+        );
     }
 }
