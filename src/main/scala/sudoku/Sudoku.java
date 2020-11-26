06ef2ef8657b28db369eba47a6b85887db387731
theo-richard <theo.richard.contact@gmail.com>
2020-11-26 12:50:11 +0100

    Sudoku resolution working! TODO: * Need to implement method to update maps on squares update * Refactoring: getIndexesMissingStringsInShape(), getMissingStringsInShape(), updateMapsFromXX()

--- src/main/scala/sudoku/Sudoku.java
+++ src/main/scala/sudoku/Sudoku.java
@@ -12,18 +12,13 @@ import java.util.Map;
 public class Sudoku {
     static Map<Integer, ArrayList<String>> lines, columns, squares;
     static FileInputStream fis;
+    static boolean incomplete = true;
 
     public static void main(String[] args) {
         init(args);
 
         populateMaps();
-
-        System.out.println("LINES:");
-        printData(lines);
-        System.out.println("\nCOLUMNS:");
-        printData(columns);
-        System.out.println("\nSQUARES:");
-        printData(squares);
+        printPopulatedMaps();
 
         resolveSudoku();
 //        saveResultsInFile();
@@ -48,35 +43,6 @@ public class Sudoku {
         }
     }
 
-    private static void populateMaps() {
-        try {
-            char c;
-            String current, previous = "";
-            int lineCount = 0, columnCount = -1;
-
-            while (fis.available() > 0) {
-                c = (char) fis.read();
-                current = String.valueOf(c);
-
-                if (current.equals("\n")) columnCount = -1;
-                if (!current.equals("|") && !current.equals("\n") && !current.equals("+") && !current.equals("-")) {
-                    columnCount++;
-                    lines.get(lineCount).add(current);
-                    columns.get(columnCount).add(current);
-                    populateSquares(lineCount, columnCount, current);
-                }
-
-                //Increment nbr of line red & register values in corresponding Map
-                if (current.equals("\n") && !previous.equals("-")) {
-                    lineCount++;
-                }
-                previous = current;
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
     public static FileInputStream openFile(String workingDir, String sudoku) {
         File file = new File(workingDir + "/" + sudoku);
         if (!file.exists()) {
@@ -96,8 +62,37 @@ public class Sudoku {
         return null;
     }
 
+    private static void populateMaps() {
+        try {
+            char c;
+            String current, previous = "";
+            int lineCount = 0, columnCount = -1;
+
+            while (fis.available() > 0) {
+                c = (char) fis.read();
+                current = String.valueOf(c);
+
+                if (current.equals("\n")) columnCount = -1;
+                if (!current.equals("|") && !current.equals("\n") && !current.equals("+") && !current.equals("-")) {
+                    columnCount++;
+                    lines.get(lineCount).add(current);
+                    columns.get(columnCount).add(current);
+                    updateSquares(lineCount, columnCount, current);
+                }
+
+                //Increment nbr of line red & register values in corresponding Map
+                if (current.equals("\n") && !previous.equals("-")) {
+                    lineCount++;
+                }
+                previous = current;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     //TODO: Simplify algo
-    private static void populateSquares(int lineCount, int columnCount, String value) {
+    private static void updateSquares(int lineCount, int columnCount, String value) {
         if (lineCount < 3) { // Fill in 3 first SQUARES
             if (columnCount < 3) squares.get(0).add(value);
             else if (columnCount < 6) squares.get(1).add(value);
@@ -113,6 +108,15 @@ public class Sudoku {
         }
     }
 
+    private static void printPopulatedMaps() {
+        System.out.println("LINES:");
+        printData(lines);
+        System.out.println("\nCOLUMNS:");
+        printData(columns);
+        System.out.println("\nSQUARES:");
+        printData(squares);
+    }
+
     private static void printData(Map<Integer, ArrayList<String>> dataMapped) {
         for (int i = 0; i < 9; i++) {
             System.out.println(dataMapped.get(i).toString());
@@ -121,32 +125,100 @@ public class Sudoku {
     }
 
     private static void resolveSudoku() {
-        int missingNumbersCount;
-        for (int i = 0; i < squares.size(); i++) {
-            missingNumbersCount = Collections.frequency(squares.get(i), "_");
+        while (incomplete) {
+            for (int i = 0; i < squares.size(); i++) {
+                resolveSingleBlankInLine(i);
+                resolveSingleBlankInColumn(i);
+//                resolveSingleBlankInSquare(i);
+            }
+            hasBeenCompleted();
+        }
 
-            if (missingNumbersCount == 1) {
-                squares.get(i).set(getIndexesMissingStringsInShape(squares.get(i)).get(0), getMissingStringsInShape(squares.get(i)).get(0));
-                if (i == 1) {
-                    System.out.println("\nSQUARES:");
-                    printData(squares);
-                    break;
-                }
-            } else if (missingNumbersCount > 1) {
-                ArrayList<Integer> emptySpacesIndexes = getIndexesMissingStringsInShape(squares.get(i));
-                ArrayList<String> missingStrings = getMissingStringsInShape(squares.get(i));
-//                for (int j = 0; j < emptySpacesIndexes.size(); j++) {
-//                    for (int k = 0; k < missingStrings.size(); k++) {
-//                        if (!isInSquare(i, missingStrings.get(k)) && !isInLine(emptySpacesIndexes.get(k), missingStrings.get(k)) && isInColumn()) {
-//                            //Update maps
-//                            missingStrings.remove(k);
-//                            k = 0;
-//                            break;
-//                        }
-//                    }
-//                }
+        System.out.println("\n\n\n-----SUDOKU RESOLVED-----");
+        System.out.println("\nLINES:");
+        printData(lines);
+    }
+
+    private static void hasBeenCompleted() {
+        int i = 0;
+        boolean done;
+        while (i < 8 && missingNumbersCount(squares, i) == 0) i++;
+        done = missingNumbersCount(squares, i) == 0;
+        incomplete = !done;
+    }
+
+    private static void resolveSingleBlankInColumn(int i) {
+        boolean updateNeeded = resolveSingleMissingNumberIn(columns, i);
+        if (updateNeeded) updateMapsFromColumns();
+    }
+
+
+    private static void resolveSingleBlankInLine(int i) {
+        boolean updateNeeded = resolveSingleMissingNumberIn(lines, i);
+        if (updateNeeded) updateMapsFromLines();
+    }
+
+    private static void resolveSingleBlankInSquare(int i) {
+        boolean updateNeeded = resolveSingleMissingNumberIn(squares, i);
+        if (updateNeeded) updateMapsFromSquares();
+    }
+
+    private static void updateMapsFromColumns() {
+        for (int i = 0; i < columns.size(); i++) {
+            lines.get(i).clear();
+            squares.get(i).clear();
+        }
+
+        for (int columnI = 0; columnI < columns.size(); columnI++) {
+            for (int lineI = 0; lineI < columns.size(); lineI++) {
+                String value = columns.get(columnI).get(lineI);
+                lines.get(lineI).add(value);
+                updateSquares(lineI, columnI, value);
             }
         }
+    }
+
+    private static void updateMapsFromLines() {
+        for (int i = 0; i < lines.size(); i++) {
+            columns.get(i).clear();
+            squares.get(i).clear();
+        }
+
+        for (int lineI = 0; lineI < lines.size(); lineI++) {
+            for (int columnI = 0; columnI < lines.size(); columnI++) {
+                String value = lines.get(lineI).get(columnI);
+                columns.get(columnI).add(value);
+                updateSquares(lineI, columnI, value);
+            }
+        }
+    }
+
+    private static void updateMapsFromSquares() {
+        for (int i = 0; i < squares.size(); i++) {
+            lines.get(i).clear();
+            columns.get(i).clear();
+        }
+
+        for (int lineI = 0; lineI < squares.size(); lineI++) {
+            for (int columnI = 0; columnI < squares.size(); columnI++) {
+
+            }
+        }
+    }
+
+    private static boolean resolveSingleMissingNumberIn(Map<Integer, ArrayList<String>> shape, int shapeIndex) {
+        if (missingNumbersCount(shape, shapeIndex) == 1) {
+            shape.get(shapeIndex).set(
+                    getIndexesMissingStringsInShape(shape.get(shapeIndex)).get(0),
+                    getMissingStringsInShape(shape.get(shapeIndex)).get(0)
+            );
+            return true;
+        }
+        return false;
+    }
+
+    private static int missingNumbersCount(Map<Integer, ArrayList<String>> shape, int shapeIndex) {
+        return Collections.frequency(shape.get(shapeIndex), "_");
     }
 
     public static ArrayList<Integer> getIndexesMissingStringsInShape(ArrayList<String> values) {
@@ -163,23 +235,5 @@ public class Sudoku {
             if (!values.contains(String.valueOf(i + 1))) missingNumbers.add(String.valueOf(i + 1));
         }
         return missingNumbers;
-    }
-
-    public static boolean isInSquare(int squareIndex, String searchingNumber) {
-        ArrayList<String> values = squares.get(squareIndex);
-        for (String s : values) if (s.equals(searchingNumber)) return true;
-        return false;
-    }
-
-    public static boolean isInLine(int lineIndex, String searchingNumber) {
-        ArrayList<String> values = lines.get(lineIndex);
-        for (String s : values) if (s.equals(searchingNumber)) return true;
-        return false;
-    }
-
-    public static boolean isInColumn(int columnIndex, String searchingNumber) {
-        ArrayList<String> values = columns.get(columnIndex);
-        for (String s : values) if (s.equals(searchingNumber)) return true;
-        return false;
     }
 }
